e454663a17eabf8f3fdce21befa53b443a4989c3
David Powell <david.powell@monash.edu>
2012-05-24 10:29:40 +1000

    Bump version to 1.2.06, and update ChangeLog

--- src/globals.h
+++ src/globals.h
@@ -38,7 +38,7 @@ Copyright 2007, 2008 Daniel Zerbino (zerbino@ebi.ac.uk)
 
 #define VERSION_NUMBER 1
 #define RELEASE_NUMBER 2
-#define UPDATE_NUMBER 03
+#define UPDATE_NUMBER 06
 
 #define MAXLINE 5000 
 
