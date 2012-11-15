843384fd69ad712a87de50939de21f30504c5ee1
Daniel Zerbino <dzerbino@soe.ucsc.edu>
2012-11-15 11:36:31 -0800

    Silly typo in previous commit

--- src/globals.h
+++ src/globals.h
@@ -38,7 +38,7 @@ Copyright 2007, 2008 Daniel Zerbino (zerbino@ebi.ac.uk)
 
 #define VERSION_NUMBER 1
 #define RELEASE_NUMBER 2
-#define UPDATE_NUMBER 08
+#define UPDATE_NUMBER 8
 
 #define MAXLINE 5000 
 
